9008b039841c3b49ea848e08aa5bbe1ddf702d22
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2023-06-04 23:13:20 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Check if two arrays are equal or not/check-if-two-arrays-are-equal-or-not.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		long t=sc.nextLong();
+		
+		while(t-->0)
+		{
+		  int n=sc.nextInt();
+		  long arr[]=new long[n];
+		  long brr[]=new long[n];
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      arr[i]=sc.nextLong();
+		  }
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      brr[i]=sc.nextLong();
+		  }
+		  Solution ob = new Solution();
+		  System.out.println(ob.check(arr,brr,n)==true?"1":"0");
+		  
+		  
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    //Function to check if two arrays are equal or not.
+    public static boolean check(long A[],long B[],int N)
+    {
+        List<Long> al = new ArrayList <>();
+        List<Long> all = new ArrayList <>();
+        for (Long i : A){
+            al.add(i);
+        }
+        for (Long i : B){
+            all.add(i);
+        }
+        Collections.sort(al);
+        Collections.sort(all);
+        if (al.equals(all)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
