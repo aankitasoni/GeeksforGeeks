9da117fc775c87ab2e2819af71bfc8152986364d
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2024-02-16 21:43:24 +0530

    Added Solution - GfG to GitHub

---
+++ School/Smaller and Larger/smaller-and-larger.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans[] = new Solve().getMoreAndLess(arr, n, x);
+            System.out.println(ans[0] + " " + ans[1]);
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solve {
+    int[] getMoreAndLess(int[] arr, int n, int x) {
+        // code here
+        int g = 0;
+        int s = 0;
+        for (int i =0; i < n; i++){
+            if (arr[i] <= x){
+                s++;
+            } 
+            if (arr[i] >= x){
+                g++;
+            }
+        }
+        int ans[] = new int[2];
+        ans[0] = s;
+        ans[1] = g;
+        return ans;
+    }
+}
